0b474d9adc7dbefb6f7cf4e8c89478623d5bccb4
EC2 Default User <ec2-user@ip-172-31-6-173.us-east-2.compute.internal>
2022-11-11 05:04:19 +0000

    more practice implementing react app components

---
+++ React/open_course/course_app_components/Course.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Course = ({name, parts}) => {
+
+  const total = parts.reduce((total, currObject) => {
+    return total + currObject.exercises
+  }, 0)
+
+  const nextKey = (() => {
+    let key = -1
+    return () => key -= 1
+  })()
+
+
+  return (
+    <div>
+      <h1>{name}</h1>
+      <ul>
+        {parts.map(part => {
+          return ( <li key={nextKey()}>
+           {part.name} {part.exercises}
+          </li> )
+        })}
+      </ul>
+      Total of {total} exercises
+    </div>
+  )
+}
+
+export default Course
